2f23bc654ecc425559a484cebaefe61473bfc071
Theo Vischel <theo.vischel@hmg.inpg.fr>
2007-08-08 10:40:17 +0000

    Theo's missing log message to go here...

--- lib/results_analysis/plot_Qsim_Qobs_Rain.py
+++ lib/results_analysis/plot_Qsim_Qobs_Rain.py
@@ -1,65 +1,62 @@
 import sys
 sys.path.append('C:/Theo/topkapi_model/programme/TOPKAPI_Aug07/svn_working_copy/lib/')
 
+#General module importation
 import numpy as np
 import scipy as sp
 import pylab as pl
 import datetime as dt
 from matplotlib.dates import drange,date2num,num2date,YearLocator, MonthLocator,DayLocator, HourLocator,DateFormatter
 import tables as h5
+from ConfigParser import SafeConfigParser
+config = SafeConfigParser()
 
+#Personnal module importation
 import utils as ut
 reload(ut)
 
 
-def graph(input):
-    #############################################################################################
-    path_fileQ='C:/Theo/topkapi_model/programme/TOPKAPI_Aug07/Example/run_the_model/results/'
-    path_fileQobs='C:/Theo/topkapi_model/programme/TOPKAPI_Aug07/Example/analyse_the_results/'
-    path_fileP='C:/Theo/topkapi_model/programme/TOPKAPI_Aug07/Example/run_the_model/forcing_variables/'
-    path_out='C:/Theo/topkapi_model/programme/TOPKAPI_Aug07/Example/analyse_the_results/graphics/'
-    #create path_out if it does'nt exist
-    ut.check_folder_exist(path_out)
+def graph(ini_file='plot_Qsim_Qobs_Rain.ini'):
+    
+    config.read(ini_file)
+    print 'Read the file ',ini_file
 
-    tab_fileQ=['sample_event_simulation_results.h5']
-    file_Qobs='C8H020_6h_Obs_Example.dat'
-    fileP='rainfields.h5'
-    event='sample_event'
-    title=event
+    file_Qsim=config.get('files','file_Qsim')
+    file_Qobs=config.get('files','file_Qobs')
+    file_rain=config.get('files','file_rain')
+    image_out=config.get('files','image_out')
+    
+    group_name=config.get('groups','group_name')
 
-    Qobs=True
-    Pobs=True
-    nash=True
+    Qobs=config.getboolean('flags','Qobs')
+    Pobs=config.getboolean('flags','Pobs')
+    nash=config.getboolean('flags','nash')
             
     tab_leg=['Model']
-
-    image=path_out+tab_fileQ[0].replace('.h5','_Qsim_Qobs_P_color.png')
-
-    #tab_col=['k','g','b','y','r','g','b','y','0.5']
-    tab_col=['k','g','b','y','r','g','b','y','r']
-    tab_style=['-','-','-','-','--','--','--','--','-']
+    #tab_col=['k','0.5']
+    tab_col=['k','r']
+    tab_style=['-','-']
     #tab_width=['1','2']
     tab_width=['1','1']
     #color_P='0.5'
     color_P='b'
     transparency_P=0.5#(0 for invisible)
 
+    #create path_out if it does'nt exist
+    ut.check_file_exist(image_out)
 
     #Read the obs
     #Qobs
-    ar_date,ar_Qobs=read_delta_flow(path_fileQobs+file_Qobs)
+    ar_date,ar_Qobs=read_delta_flow(file_Qobs)
     date1=dt.datetime(ar_date[0,0],ar_date[0,1],ar_date[0,2],ar_date[0,3],ar_date[0,4])
     date1prim=dt.datetime(ar_date[1,0],ar_date[1,1],ar_date[1,2],ar_date[1,3],ar_date[1,4])
     date2=dt.datetime(ar_date[-1,0],ar_date[-1,1],ar_date[-1,2],ar_date[-1,3],ar_date[-1,4])
     delta=date1prim-date1
     X= drange(date1, date2+delta, delta)
-    ndar_Q=np.zeros((len(X),len(tab_fileQ)))
-    ar_nash=np.zeros(len(tab_fileQ))
     #Rain
     if Pobs:
-        file_rain=path_fileP+fileP
         h5file_in=h5.openFile(file_rain,mode='r')
-        group='/'+event+'/'
+        group='/'+group_name+'/'
         node = h5file_in.getNode(group+'rainfall')
         ndar_rain=node.read()
         h5file_in.close()
@@ -67,12 +64,9 @@ def graph(input):
         ar_rain=np.average(ndar_rain,axis=1)
 
     #Read the simulated data Q
-    n=-1
-    for file in tab_fileQ:
-        n=n+1
-        file_h5=path_fileQ+file
-        ndar_Qc_out=ut.read_one_array_hdf(file_h5,'/Channel/','Qc_out')
-        ndar_Q[:,n]=ndar_Qc_out[:,0]
+    file_h5=file_Qsim
+    ndar_Qc_out=ut.read_one_array_hdf(file_h5,'/Channel/','Qc_out')
+    ar_Qsim=ndar_Qc_out[:,0]
 
 
     ##Graph
@@ -122,19 +116,18 @@ def graph(input):
         tab_leg.append(('Observation'))
         tab_leg=tab_leg[::-1]
         
-    for n in range(len(tab_fileQ)):
-        pl.plot(X,ndar_Q[:,n],color=tab_col[n],linestyle=tab_style[n],linewidth=tab_width[n])
 
-    for n in range(len(tab_fileQ)):
-        if nash:
-            ar_nash[n]=ut.Nash(ndar_Q[:,n],ar_Qobs)
-            pl.plot(X[0:1],ndar_Q[0:1,n],'w:')
-            tab_leg.append(('Eff= '+str(ar_nash[n])[0:5]))
+    pl.plot(X,ar_Qsim,color=tab_col[0],linestyle=tab_style[0],linewidth=tab_width[0])
+
+    if nash:
+        nash_value=ut.Nash(ar_Qsim,ar_Qobs)
+        pl.plot(X[0:1],ar_Qsim[0:1],'w:')
+        tab_leg.append(('Eff= '+str(nash_value)[0:5]))
             
     pl.xlim(X[0],X[-1])
     ytitle=r'$Q \  (m^3/s)$'
     pl.ylabel(ytitle,fontsize=18)
-    pl.title(title)
+    pl.title(group_name)
     pl.legend(tab_leg,'center')
     leg = pl.gca().get_legend()
     leg.draw_frame(False)
@@ -153,7 +146,7 @@ def graph(input):
     pl.ylim(max(ar_rain)*2,min(ar_rain))
     pl.xlim(X[0],X[-1])
 
-    pl.savefig(image)
+    pl.savefig(image_out)
 
 
 #SUBROUTINE
