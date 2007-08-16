b3eb4f16c51763e502d30c6db6955663886189c0
Theo Vischel <theo.vischel@hmg.inpg.fr>
2007-08-16 13:06:58 +0000

    run_example.py completed.

--- TOPKAPI_Example/run_example.py
+++ TOPKAPI_Example/run_example.py
@@ -8,7 +8,13 @@ import TOPKAPI # import the TOPKAPI package
 
 TOPKAPI.model.run('run_the_model/TOPKAPI.ini') # run the model using the configuration in TOPKAPI.ini
 
-# plot the simulation results using the config in plot_Qsim_Qobs_Rain.ini
+# plot the simulation results (rainfall-runoff graphics) using the config in plot_Qsim_Qobs_Rain.ini
 from TOPKAPI.results_analysis import plot_Qsim_Qobs_Rain
 
 plot_Qsim_Qobs_Rain.run('analyse_the_results/plot_Qsim_Qobs_Rain.ini')
+
+
+# plot the simulation results (soil moisture maps) using the config in plot_soil_moisture_maps.ini
+from TOPKAPI.results_analysis import plot_soil_moisture_maps
+
+plot_soil_moisture_maps.run('analyse_the_results/plot_soil_moisture_maps.ini')
