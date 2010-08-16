b786ae0b845374ca42db42ac64322d6aa9e894c5
Scott Sinclair <scott.sinclair.za@gmail.com>
2010-08-16 14:39:58 +0200

    STY: Reformat to be more pleasing on the eye

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(name='TOPKAPI',
       description='SAHG TOPKAPI model implementation',
       author='Theo Vischel & Scott Sinclair',
       author_email='theo.vischel@hmg.inpg.fr; sinclaird@ukzn.ac.za',
-      packages=['TOPKAPI', 'TOPKAPI.parameter_utils', 'TOPKAPI.results_analysis'],
+      packages=['TOPKAPI',
+                'TOPKAPI.parameter_utils',
+                'TOPKAPI.results_analysis'],
       package_dir={'TOPKAPI':'lib'}
       )
 
