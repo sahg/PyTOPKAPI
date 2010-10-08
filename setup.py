dd75c28a12e493f635196c7297c6a795620a59c6
Scott Sinclair <scott.sinclair.za@gmail.com>
2010-10-08 13:55:01 +0200

    DOC: Forgot to add HESS reference to setup.py long description

--- setup.py
+++ setup.py
@@ -67,7 +67,8 @@ Hydrological model (Liu and Todini, 2002). The model is a
 physically-based and fully distributed hydrological model, which has
 already been successfully applied in several countries around the
 world (Liu and Todini, 2002; Bartholomes and Todini, 2005; Liu et al.,
-2005; Martina et al., 2006; Vischel et al., 2008).
+2005; Martina et al., 2006; Vischel et al., 2008, Sinclair and Pegram,
+2010).
 
 """,
           license='BSD',
