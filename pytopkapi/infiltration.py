8593c0e2c0edaab1269e1aa1e21627afa4acbcdc
Scott Sinclair <scott.sinclair.za@gmail.com>
2010-10-19 16:11:42 +0200

    ENH: Add function to compute Green-Ampt infiltraton rate

--- pytopkapi/infiltration.py
+++ pytopkapi/infiltration.py
@@ -56,8 +56,49 @@ def green_ampt_cum_infiltration(psi, eff_theta, eff_sat, K, t):
     else:
         raise ValueError(mesg)
 
+def green_ampt_infiltration_rate(psi, eff_theta, eff_sat, K, t):
+    """Compute the Green-Ampt infiltration rate
+
+    Compute the potential infiltration rate at time `t`, using
+    Green-Ampt.
+
+    Parameters
+    ----------
+    psi : scalar
+        Soil suction head at wetting front.
+    eff_theta : scalar
+        Effective porosity.
+    eff_sat : scalar
+        Effective saturation.
+    K : scalar
+        Saturated hydraulic conductivity.
+    t : scalar
+        Time since beginning of event
+
+    Returns
+    -------
+    ft : scalar
+        Infiltration rate at time `t`.
+
+    Raises
+    ------
+    ValueError - If no solution can be found.
+
+    """
+
+    F = green_ampt_cum_infiltration(psi, eff_theta, eff_sat, K, t)
+
+    dtheta = (1 - eff_sat)*eff_theta
+
+    return K*((psi*dtheta)/F + 1)
+
 def test_basic_green_ampt():
-    """Test the Green-Ampt cumulative infiltration solution"""
+    """Test the Green-Ampt infiltration solution
+
+    This test implements Example 4.3.1 pg 116 from Chow et al. 1988,
+    'Applied Hydrology'
+
+    """
 
     psi = 16.7
     eff_theta = 0.486
@@ -65,6 +106,10 @@ def test_basic_green_ampt():
     K = 0.65
     t = 1
 
-    result = green_ampt_cum_infiltration(psi, eff_theta, eff_sat, K, t)
+    cum_infil = green_ampt_cum_infiltration(psi, eff_theta, eff_sat, K, t)
 
-    assert np.allclose(result, [3.16721373])
+    assert np.allclose(cum_infil, [3.16721373])
+
+    infil_rate = green_ampt_infiltration_rate(psi, eff_theta, eff_sat, K, t)
+
+    assert np.allclose(infil_rate, [1.81596836])
