159dea01c64fb7c446686be1370963232ddf014b
Scott Sinclair <scott.sinclair.za@gmail.com>
2010-12-23 14:01:51 +0200

    ENH: Provide a generic function to extract SSI for the full simulation

--- pytopkapi/results_analysis/sim_result_tools.py
+++ pytopkapi/results_analysis/sim_result_tools.py
@@ -1,6 +1,7 @@
-__all__ = ['extract_ssi']
+__all__ = ['extract_ssi', 'extract_ssi_to_file']
 
 from datetime import timedelta
+from ConfigParser import SafeConfigParser
 
 import h5py
 import numpy as np
@@ -9,7 +10,57 @@ import numpy.ma as ma
 # gzip compression flag
 comp = 6
 
-def extract_ssi(sim_fname, param_fname, result_fname, start_dt):
+def extract_ssi(control_fname):
+    """Extract SSI from a PyTOPKAPI simulation file.
+
+    Read a PyTOPKAPI simulation file and it's associated parameter
+    file and compute the Soil Saturation Index (SSI) for each model
+    cell and timestep. The results are returned as a Numpy array.
+
+    Parameters
+    ----------
+    control_fname : string
+        The file name of a PyTOPKAPI simulation control file. The name
+        should contain the full path relative to the current
+        directory.
+
+    Returns
+    -------
+    ssi : Numpy ndarray
+        A Numpy array containing the calculated SSI values.
+
+    """
+    config = SafeConfigParser()
+    config.read(control_fname)
+
+    global_param_fname = config.get('input_files', 'file_global_param')
+    param_fname = config.get('input_files', 'file_cell_param')
+    sim_fname = config.get('output_files', 'file_out')
+    fac_L = config.getfloat('calib_params', 'fac_L')
+
+    params = np.loadtxt(param_fname)
+    glob_params = np.genfromtxt(global_param_fname, names=True)
+
+    soil_depth = fac_L*params[:, 8]
+    factor = params[:, 11] - params[:, 10]
+    cell_area = glob_params['X']**2 # m^2
+
+    soil_depth = ma.masked_values(soil_depth, 0.0)
+    factor = ma.array(factor, mask=soil_depth.mask)
+    div = factor*soil_depth*cell_area
+
+    tkpi_file = h5py.File(sim_fname)
+    soil_vol = tkpi_file['/Soil/V_s'][...]
+    tkpi_file.close()
+
+    # ssi = (Vs/cell_vol)*100
+    # cell_vol = (theta_s - theta_r)*soil_depth*cell_area
+    sv = ma.array(soil_vol, mask=soil_depth.mask)
+    ssi = (sv/(div))*100.0
+
+    return ssi
+
+def extract_ssi_to_file(sim_fname, param_fname, result_fname, start_dt):
     """
     Read a TOPKAPI simulation file and it's associated parameter file
     and compute the SSI for each timestep. Store the results in a new
