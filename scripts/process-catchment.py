61be74e24aafe59813e255155a52fc6dd4047605
Scott Sinclair <scott.sinclair.za@gmail.com>
2011-11-03 15:03:41 +0200

    ENH: Allow processing for a variety of tertiary catchments

--- scripts/process-catchment.py
+++ scripts/process-catchment.py
@@ -2,6 +2,8 @@ import grass.script as grass
 
 accum_thresh = 30
 
+tertiary = 'B60'
+
 # make sure the region is set correctly
 grass.run_command('g.region', rast='srtm-dem')
 
@@ -9,76 +11,79 @@ grass.run_command('g.region', rast='srtm-dem')
 grass.run_command('v.extract',
                   flags = '-o',
                   input = 'sa_quaternaries',
-                  output = 'liebenbergsvlei_py',
-                  where="TERTIARY = 'C83'")
+                  output = '%s_py' % tertiary,
+                  where="TERTIARY = '%s'" % tertiary)
 
 grass.run_command('v.dissolve',
                   flags = '-o',
-                  input = 'liebenbergsvlei_py',
-                  output = 'lieb_dissolve_py',
+                  input = '%s_py' % tertiary,
+                  output = '%s_dissolve_py' % tertiary,
                   column='TERTIARY')
 
 grass.run_command('v.buffer',
                   flags = '-o',
-                  input = 'lieb_dissolve_py',
-                  output = 'lieb_buffer_py',
+                  input = '%s_dissolve_py' % tertiary,
+                  output = '%s_buffer_py' % tertiary,
                   distance='5000.0')
 
 grass.run_command('v.to.rast',
                   flags = '-o',
-                  input = 'lieb_buffer_py',
-                  out = 'lieb_mask_py',
+                  input = '%s_buffer_py' % tertiary,
+                  out = '%s_mask_py' % tertiary,
                   use = 'val')
 
 # use mask to extract catchment from DEM
-grass.run_command('r.mask', input='lieb_mask_py')
-grass.mapcalc('lieb_dem=srtm_dem', overwrite=True)
+grass.run_command('r.mask', input='%s_mask_py' % tertiary)
+grass.mapcalc('%s_dem=srtm_dem' % tertiary, overwrite=True)
 grass.run_command('r.mask', flags='r')
 
 # obtain parameters from DEM
 grass.run_command('r.slope.aspect',
                   flags = '-o',
-                  elevation = 'lieb_dem',
-                  slope = 'lieb_slope')
+                  elevation = '%s_dem' % tertiary,
+                  slope = '%s_slope' % tertiary)
 
 grass.run_command('r.watershed',
                   flags = '-o',
-                  elevation = 'lieb_dem',
-                  accumulation = 'lieb_accum',
-                  drainage = 'lieb_dir',
+                  elevation = '%s_dem' % tertiary,
+                  accumulation = '%s_accum' % tertiary,
+                  drainage = '%s_dir' % tertiary,
                   threshold=250)
 
-grass.mapcalc('lieb_accum_thresh=abs(lieb_accum) >= %s' % accum_thresh,
+grass.mapcalc('%s_accum_thresh=abs(%s_accum) >= %s' % (tertiary, tertiary, accum_thresh),
               overwrite=True)
 
-grass.mapcalc('lieb_accum_abs=abs(lieb_accum)', overwrite=True)
+grass.mapcalc('%s_accum_abs=abs(%s_accum)' % (tertiary, tertiary),
+              overwrite=True)
 
 # crop region to mask
-grass.run_command('g.region', zoom='lieb_mask_py', align='lieb_mask_py')
+grass.run_command('g.region',
+                  zoom='%s_mask_py' % tertiary,
+                  align='%s_mask_py' % tertiary)
 
 # output to GTiff for further processing
 grass.run_command('r.out.gdal',
                   type='Int16',
-                  input='lieb_dir',
+                  input='%s_dir' % tertiary,
                   format='GTiff',
                   output='lieb-flow-dir.tif')
 
 grass.run_command('r.out.gdal',
-                  input='lieb_mask',
+                  input='%s_mask_py' % tertiary,
                   format='GTiff',
                   output='lieb-mask.tif')
 
 grass.run_command('r.out.gdal',
-                  input='lieb_slope',
+                  input='%s_slope' % tertiary,
                   format='GTiff',
                   output='lieb-slope.tif')
 
 grass.run_command('r.out.gdal',
-                  input='lieb_accum_abs',
+                  input='%s_accum_abs' % tertiary,
                   format='GTiff',
                   output='lieb-accum-abs.tif')
 
 grass.run_command('r.out.gdal',
-                  input='lieb_accum_thresh',
+                  input='%s_accum_thresh' % tertiary,
                   format='GTiff',
                   output='lieb-accum-thresh.tif')
