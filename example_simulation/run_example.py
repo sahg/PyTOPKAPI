88e4c22ab8d6fae06d9b041a8e16d3b0b2c848cd
Scott Sinclair <scott.sinclair.za@gmail.com>
2012-02-06 16:46:56 +0200

    REF: Reorganize script to run example

--- example_simulation/run_example.py
+++ example_simulation/run_example.py
@@ -1,17 +1,16 @@
 import pytopkapi
+from pytopkapi.results_analysis import plot_Qsim_Qobs_Rain, \
+                                       plot_soil_moisture_maps
 
-# run a model simulation using the configuration in TOPKAPI.ini
-pytopkapi.run('run_the_model/TOPKAPI.ini')
+# Run a model simulation using the configuration in
+# model-simulation.ini
+pytopkapi.run('model-simulation.ini')
 
-# plot the simulation results (rainfall-runoff graphics) using the
-# config in plot_Qsim_Qobs_Rain.ini
-from pytopkapi.results_analysis import plot_Qsim_Qobs_Rain
-
-plot_Qsim_Qobs_Rain.run('analyse_the_results/plot_Qsim_Qobs_Rain.ini')
+# Plot the simulation results (rainfall-runoff graphics) using the
+# config in plot-flow-precip.ini
+plot_Qsim_Qobs_Rain.run('plot-flow-precip.ini')
 
 
-# plot the simulation results (soil moisture maps) using the config in
-# plot_soil_moisture_maps.ini
-from pytopkapi.results_analysis import plot_soil_moisture_maps
-
-plot_soil_moisture_maps.run('analyse_the_results/plot_soil_moisture_maps.ini')
+# Plot the simulation results (soil moisture maps) using the config in
+# plot-soil-moisture-maps.ini
+plot_soil_moisture_maps.run('plot-soil-moisture-maps.ini')
