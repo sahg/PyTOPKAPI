2a0bb74e9a3bfdb262aed1d9628440ea8859bf3f
Scott Sinclair <scott.sinclair.za@gmail.com>
2017-06-12 17:32:58 +0200

    Edit banner text

--- pytopkapi/utils.py
+++ pytopkapi/utils.py
@@ -5,15 +5,18 @@ from subprocess import Popen, PIPE
 import h5py
 import numpy as np
 
+import pytopkapi
+
 def show_banner():
     """Show an ASCII banner describing the model.
 
     """
 
-    print("""=====================================================================
-PyTOPKAPI - a Python implementation of the TOPKAPI Hydrological model
-=====================================================================
-""")
+    print('===============================================================\n',
+          '\n PyTOPKAPI\n',
+          'A Python implementation of the TOPKAPI Hydrological model\n\n',
+          'Version {}\n'.format(pytopkapi.__version__),
+          '\r===============================================================\n')
 
 # System utility functions
 def exec_command(cmd_args):
