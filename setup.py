0fc40c7448cc53d79aca479a78aee4ee02f927b3
Scott Sinclair <scott.sinclair.za@gmail.com>
2017-06-19 11:19:14 +0200

    Update minor version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 from distutils.core import setup
 
 MAJOR               = 0
-MINOR               = 3
+MINOR               = 4
 MICRO               = 0
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
