c5bc74e443f3172fe267512054d3b3fb62a036fd
Scott Sinclair <scott.sinclair.za@gmail.com>
2017-06-28 14:29:08 +0200

    BUG: Under Windows, multiprocessing code entry point must be protected

--- example_simulation/run_example.py
+++ example_simulation/run_example.py
@@ -2,15 +2,16 @@ import pytopkapi
 from pytopkapi.results_analysis import plot_Qsim_Qobs_Rain, \
                                        plot_soil_moisture_maps
 
-# Run a model simulation using the configuration in
-# model-simulation.ini
-pytopkapi.run('model-simulation.ini')
+if __name__ == '__main__':
+    # Run a model simulation using the configuration in
+    # model-simulation.ini
+    pytopkapi.run('model-simulation.ini')
 
-# Plot the simulation results (rainfall-runoff graphics) using the
-# config in plot-flow-precip.ini
-plot_Qsim_Qobs_Rain.run('plot-flow-precip.ini')
+    # Plot the simulation results (rainfall-runoff graphics) using the
+    # config in plot-flow-precip.ini
+    plot_Qsim_Qobs_Rain.run('plot-flow-precip.ini')
 
 
-# Plot the simulation results (soil moisture maps) using the config in
-# plot-soil-moisture-maps.ini
-plot_soil_moisture_maps.run('plot-soil-moisture-maps.ini')
+    # Plot the simulation results (soil moisture maps) using the config in
+    # plot-soil-moisture-maps.ini
+    plot_soil_moisture_maps.run('plot-soil-moisture-maps.ini')
